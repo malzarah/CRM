5a6aa3e70b6626c363170b8a9f7cc74c1ea9f017
malzarah <malzarah@unomaha.edu>
2019-11-11 21:52:46 -0600

    serializer

--- mfscrm/crm/urls.py
+++ mfscrm/crm/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
+from rest_framework.urlpatterns import format_suffix_patterns
 app_name = 'crm'
 urlpatterns = [
     path('', views.home, name='home'),
@@ -18,5 +19,7 @@ path('product_list', views.product_list, name='product_list'),
 path('product/create/', views.product_new, name='product_new'),
 path('product/<int:pk>/edit/', views.product_edit, name='product_edit'),
 path('product/<int:pk>/delete/', views.product_delete, name='product_delete'),
-url('login/',auth_views.LoginView.as_view(), name='login')
+url('login/',auth_views.LoginView.as_view(), name='login'),
+    url(r'^customers_json/', views.CustomerList.as_view()),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
